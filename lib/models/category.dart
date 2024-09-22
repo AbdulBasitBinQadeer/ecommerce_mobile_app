f664879e706b5c065952921c34c78e7d6fa507f9
AbdulBasitBinQadeer <abdulbasitbinqadeer121@gmail.com>
2024-09-23 01:40:40 +0500

    Initial commit

---
+++ lib/models/category.dart
@@ -0,0 +1,28 @@
+class Category {
+  final String title;
+
+  Category({
+    required this.title,
+  });
+}
+
+final List<Category> categoriesList = [
+  Category(
+    title: "All",
+  ),
+  Category(
+    title: "Shoes",
+  ),
+  Category(
+    title: "Beauty",
+  ),
+  Category(
+    title: "Women's\nFashion",
+  ),
+  Category(
+    title: "Jewelry",
+  ),
+  Category(
+    title: "Men's\nFashion",
+  ),
+];
